1a0234d30f4e4a54afb061abd1d9716bc798be41
Rob Raymond <rob.raymond@hp.com>
2015-05-17 11:06:10 -0600

    Add length method to fastaTool

--- fastaTool.py
+++ fastaTool.py
@@ -4,15 +4,21 @@ import sys
 
 def usage():
     print("fastaTools find file start length")
+    print("fastaTools len file")
 
 
 def main():
     if len(sys.argv) == 5 and sys.argv[1] == 'find':
         handle = open(sys.argv[2])
+        records = list(SeqIO.parse(handle, 'fasta'))
         start = int(sys.argv[3])
         length = int(sys.argv[4])
-        records = list(SeqIO.parse(handle, 'fasta'))
         print(records[0][start:start+length].seq)
+    elif len(sys.argv) == 3 and sys.argv[1] == 'len':
+        handle = open(sys.argv[2])
+        records = list(SeqIO.parse(handle, 'fasta'))
+        for record in records:
+            print(len(record.seq))
     else:
         usage()
 
