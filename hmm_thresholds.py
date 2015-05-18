2a7a2ddaf44de420fbae1e4d76922c89cecbd076
Rob Raymond <rob.raymond@hp.com>
2015-05-18 15:07:33 -0600

    Fix to avoid multiple copies of values

--- hmm_thresholds.py
+++ hmm_thresholds.py
@@ -2,8 +2,12 @@ import sys
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
+hmm_to_score = {}
 for line in lines:
     hmm, score = line.split(',')
+    if hmm not in hmm_to_score:
+        hmm_to_score[hmm] = score
+for hmm, score in hmm_to_score.items():
     filename = '%s/%s.hmm' % (sys.argv[2], hmm)
     with open(filename) as f:
         lines = f.readlines()
