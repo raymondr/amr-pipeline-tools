1646377f78f6ab3483ea81860d282673c861e360
Rob Raymond <rob.raymond@hp.com>
2015-09-30 16:57:05 -0600

    Change bait_generation to generate kmers using Bio package

--- bait_generation.py
+++ bait_generation.py
@@ -3,6 +3,8 @@
 import argparse
 import collections
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import DNAAlphabet
 import evaluate_bait
 
 
@@ -20,6 +22,8 @@ def main():
                         help='The length of the kmers')
     parser.add_argument('-i', dest='input_file', required=True,
                         help='The name of the input fasta file')
+    parser.add_argument('-r', dest='reverse', default=False, action='store_true',
+                        help='Whether to generate reverse complement kmers as well')
     #parser.add_argument('-o', dest='output_dir', required=True,
     #                    help='The directory in which to write output files')
     args = parser.parse_args()
@@ -28,7 +32,7 @@ def main():
     kmer_count = collections.defaultdict(list)
     total_kmers = 0
     for idx, read in enumerate(reads):
-        for kmer in kmers(read.seq, args.k):
+        for kmer in kmers(read.seq, int(args.k)):
                 kmer_count[str(kmer)].append(idx)
                 total_kmers += 1
     print(len(kmer_count), total_kmers)
@@ -36,22 +40,12 @@ def main():
     # collapse kmers
     items = list(kmer_count.items())
     items.sort(key=lambda l: len(l[1]), reverse=True)
-    for i1 in range(len(items)-1):
-        for i2 in range(i1+1, len(items)):
-            """
-            item1 = items[i1]
-            item2 = items[i2]
-            if item1 and item2 and evaluate_bait.match(item1[0], item2[0]):
-                kmer_count[item1[0]].append(item2[1])
-                del kmer_count[item2[0]]
-                items[i2] = None
-            """
-            pass
-        print(i1)
 
     with open("bait.fasta", "w") as f:
         for k in kmer_count.keys():
             f.write(k + '\n')
+            if args.reverse:
+                f.write(str(Seq(k, DNAAlphabet()).reverse_complement()) + '\n')
 
 
 if __name__ == '__main__':
